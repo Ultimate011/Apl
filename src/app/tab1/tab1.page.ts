ed0248604afb14d95b423f308ae5f39292f70693
Alnosss <gamingalno@gmail.com>
2022-04-07 14:50:40 +0200

    Ny sida och började arbeta på theme switcher

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,3 +1,4 @@
+import { ThemeService } from './../services/theme.service';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,6 +8,14 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  constructor(private theme: ThemeService) {}
 
+  enableDark() {
+    this.theme.enableDark();
+  }
+  
+  
+  enableLight() {
+    this.theme.enableLight();
+  }
 }
